b6a7739224b78ffb2f10437ab5980d3d6923229f
Victor Chweya <vchweya@avicdesign.com>
2017-11-16 17:06:44 +0300

    added terms.php

---
+++ terms.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>PMVA 2017</title>
+    <base href="./">
+
+    <!-- stylesheets -->
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/colors.css">
+  </head>
+  <body id="home">
+    <nav class="navbar navbar-expand  navbar-dark flex-column flex-md-row  navbar-transparent bg-primary">
+				<a class="navbar-brand  pl-4" href="#home"><img src="img/logo.png" width="200" height="32" class="d-inline-block align-top" alt=""></a>
+			
+      <div class="navbar-scroll ml-auto">
+      	<ul class="navbar-nav flex-row">
+      	  <li class="nav-item">
+      	    <a class="nav-link active" href="./#nominees">Nominees</a>
+      	  </li>
+      	  <li class="nav-item">
+      	    <a class="nav-link active" href="terms.php">Terms & Condition</a>
+					</li>
+      	</ul>
+      </div>
+    </nav>
+    <div class="wrapper">
+      <!--header section-->
+     
+			<section id="winners2016" class="bg-alt">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-8 col-md-offset-4">
+								<h1 class="display-4">TERMS AND CONDITIONS OF THE NAIROBIAN PERSON OF THE YEAR 2017</h1>
+								<h2>Overview</h2>
+								<ul>
+									<li>The Nairobian is Kenya’s leading entertainment newspaper that delivers on aspirations of city dwellers.</li>
+									<li>The paper has a strong bias for bold and entertaining news features about city lifestyle, fashion, sports and exclusive, consummate human-interest stories.</li>
+									<li>It is published every Friday and costs only Kshs 50.
+									From 2016, The Nairobian started a tradition of recognizing personalities, ideas or objects that have had the most influence in a year, for better or worse, a tradition similar to that of leading international publication.</li>
+								</ul>
+									<h2>VOTING TERMS AND CONDITIONS</h2>
+								<ul>
+									<li>
+									<p>Interactive audience voting will be set-up by The Nairobian to enable fans to vote for their favourite contestant among the nominees of the Nairobian Person of the year 2017. The voting has been set up to encourage public participation in the Competition.</p>
+									</li>
+									<li>
+									<p>The Public will be called upon to vote for their preferred candidate. This will be done through SMS sent to 21585. There will not be any fee per vote entered by the public.</p>
+									</li>
+								</ul>
+									<h3>SMS Voting</h3>
+								<ol>
+									<li>
+									<p>You may vote by sending an SMS vote for your favourite candidate. You will have to text their names to 21585. The fee per vote entered will be Kshs. 1 and bundled SMS DO NOT APPLY.</p>
+									</li>
+									<li>
+									<p>Unrecognised SMSs (SMSs where the text does not match the required voting text format or cannot be allocated to a specific Entrant) will be charged for but not counted and the subscriber who sent the SMS will not be eligible for any of the prizes.</p>
+									</li>
+									<li>
+									<p>Voting starts on 17th November 2017 at 0000 (EAT) and ends on 28th November 2017 at 0000 (EAT).  Votes made or received outside of these times will not be counted.</p>
+									</li>
+									<li>
+									<p>Participants are not allowed to use machines that automatically send SMS messages or automate/facilitate the answer process or any other alternative way of sending SMS messages, other than a mobile phone containing a registered SIM card. The Nairobian has the right to disqualify any Participant it suspects of violating this condition.</p>
+									</li>
+									<li>
+									<p>The Nairobian reserves the right to change these Terms and Conditions at any time, and may suspend, withdraw or re-set voting at its sole discretion. Where The Nairobian does change these Terms and Conditions, this will be communicated on The Nairobian Kenya website <a href="www.sde.co.ke/nairobianpersonoftheyear">Nairobian Person of the year</a></p>
+									</li>
+									<li>
+									<p>The Nairobian reserves the right to disqualify votes if it has reasonable grounds to suspect that there has been any attempt to manipulate the voting fraudulently.</p>
+									</li>
+									<li>
+									<p>After the SMS voting has closed, the result of the vote will be established from all votes received. Late votes, or votes not received by the time the result is calculated, will not be counted but will be charged by your service provider where applicable.</p>
+									</li>
+									<li>
+									<p>The Nairobian, including their holding and subsidiary companies, affiliates, employees, agents and sub-contractors (“the Indemnified Persons”) do not accept any responsibility whatsoever for any technical failure or malfunction or any other problem with any mobile network, system, server, provider, software or otherwise which may result in any vote being lost or not properly received, registered or recorded or not being received on or before the closing time.</p>
+									</li>
+									<li>
+									<p>The Nairobian is committed to a fair voting process and to preventing abuse of its systems, as well as those of its agents, affiliates and sub-contractors. However, The Nairobian does not guarantee that the mobile network and other networks used in connection with voting will be free from errors or omissions nor that they will be available uninterrupted and in a fully operating condition. In the case of system failure or repair or for reasons beyond the reasonable control of The Nairobian, the system and/or voting service may be suspended temporarily and without notice. The Indemnified Persons will not be liable to any person in the event that all or any part of the system and/or voting service is discontinued, modified or interrupted in any way.</p>
+									</li>
+									<li>
+									<p>The Nairobian will have a say in the outcome of the vote. The contestant with the most votes and best judged candidate will emerge as the winners. In the event that the vote outcome cannot be reasonably determined (due to a technical failure or other circumstance beyond The Nairobian’s reasonable control and the data is irrecoverable), then The Nairobian may postpone the result or exclude the results from the system that failed, unless the data can be recovered.</p>
+									</li>
+									<li>
+									<p>The Nairobian reserves the right to amend these terms and conditions on notice. Any change will be posted on this website and such posting shall be adequate notice to all participants in the voting. Accordingly, you hereby undertake to check these Terms and Conditions each time you wish to vote.</p>
+									</li>
+									<li>
+									<p>These Terms and Conditions shall be governed by the Laws of Kenya.</p>
+									</li>
+								</ol>
+								<h3>ONLINE VOTING:</h3>
+								<ol>
+									<li>You may vote by logging onto <a href="www.sde.co.ke/nairobianpersonoftheyear">Nairobian Person of the year</a>  to vote for your preferred candidate.</li>
+									<li>You can vote using any device that is a laptop, a PC, a tablet or an Android phone or IPhone a maximum of 4devices.</li>
+									<li>You can vote as many times as possible.</li>
+									<li>Once one votes, one is required to verify their vote by accessing their emails and follow the instructions.</li>
+									<li>Voting starts at 17th November 2017 at 0000 (EAT) and ends on 28th November 2017 at 0000 (EAT).  Votes made or received outside of these times will not be counted.</li>
+								</ol>
+								<p><strong>CAUTION: The Nairobian does not require subscribers to send airtime, money or any other consideration in order to collect prizes and will not be liable for any losses or other damage.</strong></p>
+
+							</div>
+						</div>	    		
+					</div>
+			</section>
+
+
+		 </div><!-- end wrapper -->
+		 <footer>
+			 <section class="colored-section bg-dark" id="download">
+						<div class="container">
+								
+								<div class="row">
+									<div class="col-md-12 text-center mb-4">
+										<h3>Contact us in case of sponsorships or partnerships.</h3>
+									</div>
+									<div class="col-md-3">
+										<p>Christopher Masira</p>
+										<address><i class="fa fa-mobile"></i> : 0727-929712</address>
+										<p><i class="fa fa-envelope-o"></i> <a href="mailto:cmasira@standardmedia.co.ke">cmasira@standardmedia.co.ke</a></p>
+									</div>
+									<div class="col-md-3">
+									 <p>Frank Wambua</p>
+										<address><i class="fa fa-mobile"></i> : 0721-804302</address>
+										<p><i class="fa fa-envelope-o"></i> <a href="mailto:fwambua@standardmedia.co.ke">fwambua@standardmedia.co.ke</a></p> 
+									</div>
+									<div class="col-md-3">
+									 <p>Chris Okeno</p>
+										<address><i class="fa fa-mobile"></i> : 0720-917272</address>
+										<p><i class="fa fa-envelope-o"></i> <a href="mailto:cokeno@standarmedia.co.ke"> cokeno@standarmedia.co.ke</a></p>
+									</div>
+									<div class="col-md-3">
+									 <p>Steve Ogolla</p>
+										<address><i class="fa fa-mobile"></i> : 0722-881816</address>
+										<p><i class="fa fa-envelope-o"></i> <a href="mailto:cokeno@standarmedia.co.ke"> sogola@standardmedia.co.ke</a></p>
+									</div>	
+
+										<div class="col-md-12 text-center mt-5">
+												
+												<p>
+														<a target="_blank" href="#" data-toggle="tooltip" data-placement="top" data-title="Tweet" class="btn btn-white btn-circle btn-twitter" data-original-title="" title=""><em class="fa fa-twitter"></em></a>
+														<a target="_blank" href="#" data-toggle="tooltip" data-placement="top" data-title="Share" class="btn btn-white btn-circle btn-facebook" data-original-title="" title=""><em class="fa fa-facebook"></em></a>
+														<a target="_blank" href="#" data-toggle="tooltip" data-placement="top" data-title="Share" class="btn btn-white btn-circle btn-linkedin" data-original-title="" title=""><em class="fa fa-instagram"></em></a>
+												</p>
+												<div class="clearfix"></div>
+												<br>
+												<p class=" text-muted px-5">
+														© 2017 Standard Digital - The Nairobian - 
+														<a href="https://sde.co.ke">sde.co.ke</a>.
+												</p>
+										</div>
+								</div>
+						</div>
+				</section>
+		 </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/popper.js/dist/umd/popper.min.js"></script>
+		<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
